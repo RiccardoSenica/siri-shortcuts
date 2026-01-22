73a4b65936fb10d8b8f1e7545427f12034d15549
Riccardo Senica <riccardo@frompixels.com>
2026-01-22 19:03:51 +0100

    chore: gitea changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
@@ -10,6 +11,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
       <body>{children}</body>
+      <Script
+        defer
+        src='https://analytics.frompixels.com/script.js'
+        data-website-id='af4f32d9-d524-44cc-ae9b-c88a0beec398'
+      />
     </html>
   );
 }
